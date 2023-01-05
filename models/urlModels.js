645c486a2c3c1d29cca5819b2587f441a0c9b5e8
selim <slmgn3r@gmail.com>
2023-01-05 22:08:56 +0300

    date to string

--- models/urlModels.js
+++ models/urlModels.js
@@ -44,7 +44,7 @@ urlSchema.statics.urlKısalt=async function(original_url, shortened_url, special
     if (original_url==="" || end_time==="") {
         throw new Error("Lütfen tüm alanları doldurunuz")
     }
-    if (!validator.isAfter(end_time.toString(),date)) {
+    if (!validator.isAfter(end_time.toString(),Date.now().toString())) {
         throw new Error("Lütfen geçerli bir tarih giriniz")
     }
     if (!validator.isURL(original_url)) {
